e98b3ea9282348f20109cfb44db53d6b6653bfaf
alex1 <alex1@alexzen.lan>
2025-06-07 14:45:05 -0400

    round 2

---
+++ icecap-saas/src/main/sql/icecap_data_tables.sql
@@ -0,0 +1,117 @@
+use icecap;
+drop table if exists athlete;
+drop table if exists team;
+drop table if exists contract_info;
+drop table if exists league;
+drop table if exists skater_stats;
+drop table if exists goalie_stats;
+-- defines the table for a league
+create table league(
+  league_id char(36)
+, name varchar(20)
+, sport varchar(25)
+, founded int
+, commissioner varchar(50)
+, teams int
+, roster_limit int
+, salary_cap decimal
+, contracts_limit int
+, retained_limit int
+, primary key(league_id));
+-- defines data for team
+create table team(
+   team_id char (36) 
+ , in_league_id char(36)
+ , teamName varchar(255)
+ , yearFounded int
+ , conference varchar(20)
+ , division varchar(20)
+ , gmName varchar(20)
+ , coachName varchar(20)
+ , captain varchar(255)
+ , capHit decimal
+ , capSpace decimal
+ , active int
+ , retained int
+ , owner varchar(50)
+ , website varchar(255)
+ , primary key(team_id)
+ , foreign key(in_league_id) references league(league_id));
+-- defines data for
+create table athlete(
+   athlete_id varchar(255)
+ , current_team_id char(36)
+ , firstName varchar(255)
+ , lastName varchar(20)
+ , jNumber int
+ , age int
+ , pos varchar(20)
+ , heightFeet int
+ , heightInches int
+ , weightPounds int
+ , agentFirst varchar(20)
+ , agentLast varchar(20)
+ , nation varchar(20)
+ , dob varchar(100)
+ , primary key(athlete_id)
+ , foreign key(current_team_id) references team(team_id));
+
+
+
+create table contract_info(
+  contract_id  char(36) primary key
+, player_id char(36)
+, first_season int
+, agent varchar(20)
+, team char(36)
+, term int
+, value decimal
+, capHit decimal
+, percent_at_start decimal
+, status varchar(255)
+, foreign key(player_id) references athlete(athlete_id)
+, foreign key(team) references team(team_id));
+
+
+create table skater_stats(
+  unique_season_id char(36) primary key
+, skater_id char(36)
+, yearSkate int
+, team_id char(36)
+, gamesPlayed int
+, goals int
+, assists int
+, points int
+, plusMinus int
+, pentaltyMin int
+, evenStrengthGoals int
+, evenStrengthPoints int
+, powerPlayGoals int
+, powerPlayPoints int
+, shots int
+, foreign key(skater_id) references athlete(athlete_id)
+, foreign key(team_id) references team(team_id));
+
+create table goalie_stats(
+  unique_season_id char(36) primary key
+, goalie_id char(36)
+, yearSkate int
+, team_id char(36)
+, gamesPlayed int
+, gamesStarted int
+, wins int
+, losses int
+, overtimeLosses int
+, shotsAgainst int
+, saves int
+, goalsAgainst int
+, gaa decimal
+, savePercent decimal
+, shutouts int
+, foreign key(goalie_id) references athlete(athlete_id)
+, foreign key(team_id) references team(team_id));
+
+
+
+
+ 
